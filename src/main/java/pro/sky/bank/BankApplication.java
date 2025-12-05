48c01252e06f82f57d821d6781fa5d2227fdbfb3
zman43g <zman3@yandex.ru>
2025-12-05 16:29:21 +0700

    Initial bankApp

---
+++ src/main/java/pro/sky/bank/BankApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankApplication.class, args);
+	}
+
+}
